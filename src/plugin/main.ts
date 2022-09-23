7494ba07e871585615d69765dafec35397dfa5bd
unknown <elhilali.mad@gmail.com>
2022-09-23 20:24:07 +0200

    Implement delete of colors shurtcut

--- src/plugin/main.ts
+++ src/plugin/main.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-const */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {Hotkey, MarkdownView , Plugin, Setting , PluginSettingTab, App } from 'obsidian';
-import { createColorCommand, loadShortcuts} from 'src/utils/helpers';
+import { checkRegisteredCommands, createColorCommand, loadShortcuts} from 'src/utils/helpers';
 
 export default class AutoColorPlugin extends Plugin {
 
@@ -13,7 +13,7 @@ export default class AutoColorPlugin extends Plugin {
 		this.addSettingTab(new ColorSettingsTab(this.app, this));
 		// createColorCommand('chartreuse', 'auto-color:red' , 'auto red' ,  this , [{modifiers: ['Mod', 'Shift'], key: 'R'}]);
 		// createColorCommand('chocolate', 'auto-color:green' , 'auto green' ,  this ,  [{modifiers: ['Mod', 'Shift'], key: 'G'}] );
-		loadShortcuts(this);
+		await loadShortcuts(this);
 		await this.saveSettings();
 		await this.saveData(this.settings);
 	}
@@ -75,9 +75,11 @@ class ColorSettingsTab extends PluginSettingTab {
 			}
 			text.onChange(async (value) => {
 				if (value.at(-1) === '!') {
+					this.plugin.loadSettings();
 					value = value.slice(0, -1);
 					text.setValue(value);
-					loadShortcuts(this.plugin);
+					await loadShortcuts(this.plugin);
+					await checkRegisteredCommands(this.plugin);
 				}
 				this.plugin.settings.colors = value;
 				await this.plugin.saveSettings();
@@ -88,8 +90,9 @@ class ColorSettingsTab extends PluginSettingTab {
 		this.plugin.loadSettings().then(() => {
 			const colors = this.plugin.settings.colors.split(";").slice(0, -1);
 			colors.forEach((color , index) => {
-				console.log(index);
+				console.log("Processing Color : " + color);
 				if (!this.plugin.settings.registeredColors.contains(color)) {
+					console.log("Creating Color : " + color);
 					createColorCommand(color, 'auto-color:'+color , 'auto '+color , this.plugin); 
 				}
 			});
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,24 +1,28 @@
-import { Hotkey, MarkdownView, Setting } from "obsidian";
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import { Hotkey, MarkdownView, Notice, Setting } from "obsidian";
 import AutoColorPlugin from "src/plugin/main";
 import { colorKeyWords } from "./colorKeyWords";
 
-export function createColorCommand(color : string ,id : string , name : string , plugin : AutoColorPlugin ,shortcut ?: Hotkey[]): void {
+export async function createColorCommand(color : string ,id : string , name : string , plugin : AutoColorPlugin ,shortcut ?: Hotkey[]): Promise<void> {
 
     if (verifyColor(color) == undefined){
         console.log('Invalid color :' + color);
         return;
     }
+
+
     plugin.addCommand({
         id: id,
         name: name,
         hotkeys: shortcut,
-        callback: () => {
+        checkCallback: (checking: boolean) => {
+            if (checking) return true;
             changeColor(color , plugin);
-        },
+        }
     });
     console.log(color , id , name , shortcut);
     plugin.settings.registeredColors.push(color);
-    plugin.saveSettings();
+    await plugin.saveSettings();
 }
 
 export function createNewColorSettings(container : HTMLElement , plugin : AutoColorPlugin): any {
@@ -38,6 +42,10 @@ export function createNewColorSettings(container : HTMLElement , plugin : AutoCo
 }
 
 export function changeColor(color : string , plugin : AutoColorPlugin): void {
+    if (!plugin.settings.registeredColors.contains(color)){
+        new Notice("This color have been deleted - please reload the plugin");
+        return;
+    }
     const leaf = plugin.app.workspace.getActiveViewOfType(MarkdownView);
     if (!leaf) return
     const editor = leaf.editor;
@@ -74,13 +82,35 @@ export function verifyColor(color: string): string | undefined {
     }
 }
 
-export function loadShortcuts(plugin : AutoColorPlugin , value ?: string): void {
-    plugin.loadSettings().then(() => {
+export async function loadShortcuts(plugin : AutoColorPlugin , value ?: string): Promise<void> {
+    await plugin.loadSettings().then(() => {
         const colors = plugin.settings.colors.split(";");
         colors.forEach((color) => {
             if (!plugin.settings.registeredColors.contains(color)) {
                 createColorCommand(color, 'auto-color:'+color , 'auto '+color , plugin); 
             }
         });
+    });
+}
+
+export async function checkRegisteredCommands(plugin : AutoColorPlugin): Promise<void> {
+    await plugin.loadSettings()
+        .then(() => {
+            const registredCommands : string[] = plugin.settings.registeredColors;
+            const colors = plugin.settings.colors.split(";");
+            let isModifier : boolean = false;
+            registredCommands.forEach(async (color) => {
+                if (!colors.contains(color)){
+                    console.log('Modified registered colors : ' + plugin.settings.registeredColors);
+                    plugin.settings.registeredColors.remove(color);
+                    console.log('Modified registered colors after change : ' + plugin.settings.registeredColors);
+                    isModifier = true;
+                }
+                if (isModifier){
+                    console.log('color : ' + color);
+                    await plugin.saveSettings();
+                    new Notice("Some color(s) have been deleted their shortcuts don't going to work but they still in the plugin shortcuts - reload the plugin to not see them" , 10);
+                } 
+            });
     });
 }
